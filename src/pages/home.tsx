41c0e1f4ef535d8b29545a9ce5513448bc8c61d2
Victor Manuel Gracia <victorgracia2010@ol4b.com>
2024-06-07 16:20:16 -0500

    Agrego layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,57 @@
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Typography from '@mui/joy/Typography';
+import Sidebar from '../components/layout/Sidebar';
+import Header from '../components/layout/Header';
+import CustomBreadcrumbs from '../components/layout/breadcrumsContainer';
+
 const HomePage = () => {
     return (
         <>
-            Dasboard
+            <CssVarsProvider disableTransitionOnChange>
+            <CssBaseline />
+            <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+                <Header />
+                <Sidebar />
+                <Box
+                    component="main"
+                    className="MainContent"
+                    sx={{
+                        px: { xs: 2, md: 6 },
+                        pt: {
+                            xs: 'calc(12px + var(--Header-height))',
+                            sm: 'calc(12px + var(--Header-height))',
+                            md: 3,
+                        },
+                        pb: { xs: 2, sm: 2, md: 3 },
+                        flex: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        minWidth: 0,
+                        height: '100dvh',
+                        gap: 1,
+                    }}
+                >
+                    <CustomBreadcrumbs window='Vinculaciones' href='/vinculaciones'/>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            mb: 1,
+                            gap: 1,
+                            flexDirection: { xs: 'column', sm: 'row' },
+                            alignItems: { xs: 'start', sm: 'center' },
+                            flexWrap: 'wrap',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <Typography level="h2" component="h1">
+                            Home
+                        </Typography>
+                    </Box>
+                </Box>
+            </Box>
+        </CssVarsProvider>
         </>
     );
 }
